87199c83e22bd07869980a095f77138ee477c80a
Aman Wolde <aman.wolde@mavenwave.com>
2022-02-12 14:25:06 -0600

    working script i think

--- setup.ps1
+++ setup.ps1
@@ -34,68 +34,22 @@ IAcceptSQLServerLicenseTerms="True"' > c:\install.ini
 Set-Service SQLSERVERAGENT -StartupType Automatic
 Start-Service SQLSERVERAGENT
 
+# import sqlserver module
+Install-Module -Name SqlServer -force
+
+# get db backup from gcs
+gsutil cp gs://router-image-vmdk/yourdb.bak "C:\Program Files\Microsoft SQL Server\MSSQL13.MSSQLSERVER\MSSQL\Backup\yourdb.bak"
+
+# restore db
+Restore-SqlDatabase -ServerInstance localhost -BackupFile .\yourdb.bak -Database YourDB
+
 Restart-Computer
 
-Invoke-Sqlcmd -ServerInstance localhost
-   2 cat C:\install.ini
-   3 $connectionString = 'Data Source={0};database={1};User ID={2};Password={3}' -f $ServerName,$DatabaseName,$userN...
-   4 $sqlConnection = New-Object System.Data.SqlClient.SqlConnection $connectionString
-   5 $sqlConnection.Open()
-   6 Invoke-Sqlcmd -Query "CREATE DATABASE YourDB" -ServerInstance YourInstance
-   7 Invoke-Sqlcmd -Query "CREATE DATABASE YourDB" -ServerInstance localhost
-   8 Invoke-Sqlcmd -Query "CREATE DATABASE YourDB" -ServerInstance localhost
-   9 Invoke-Sqlcmd -Query "show databases" -ServerInstance localhost
-  10 Invoke-Sqlcmd -Query "select databases" -ServerInstance localhost
-  11 gsutil ls
-  12 gsutil cp gs://router-image-vmdk/employees.sql .
-  13 Restore-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile .\employees.sql
-  14 Restore-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile \\employees.sql
-  15 Restore-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile \\employees.sql
-  16 move .\employees.sql c:\
-  17 Restore-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile c:\employees.sql
-  18 Invoke-Sqlcmd -InputFile C:\employees.sql -ServerInstance localhost -Database YourDB
-  19 Invoke-Sqlcmd -Query "CREATE DATABASE employees" -ServerInstance localhost
-  20 Invoke-Sqlcmd -InputFile C:\employees.sql -ServerInstance localhost -Database YourDB
-  21 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB
-71 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query $values
-  72 echo $values
-  73 vim .\values.sql
-  74 $values=$(cat .\values.sql)
-  75 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query $values
-  76 vim .\values.sql
-  77 $values=$(cat .\values.sql)
-  78 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query $values
-  79 vim .\values.sql
-  80 vim .\values.sql
-  81 $values=$(cat .\values.sql)
-  82 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query $values
-  83 vim .\values.sql
-  84 $values=$(cat .\values.sql)
-  85 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query $values
-  86 cat .\values.sql
-  87 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query INSERT INTO employees VALUES (10001,'1953-09-02...
-  88 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query "NSERT INTO employees VALUES (10001,'1953-09-02...
-  89 Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query  "INSERT INTO employees VALUES (10001,'1953-09-...
-  90 cat .\values.sql
-  91 Backup-SqlDatabase -ServerInstance localhost -Database YourDB
-  92 dir
-  93 Backup-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile c:\mainDB.bak
-  94 Backup-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile \mainDB.bak
- 100 Backup-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile \mainDB.bak
- 101 Backup-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile ./inDB.bak
- 102 dir
- 103 Backup-SqlDatabase -ServerInstance localhost -Database YourDB -BackupFile C:\Users\aman_wolde\main.bak
- 104 dir /s
- 105 find .
- 106 find /
- 107 find
- 108 find c:
- 109  cd '.\Program Files\Microsoft SQL Server'
- 110 dir
- 111 cd .\MSSQL13.MSSQLSERVER
- 112 dir
- 113 cd .\MSSQL
- 114 dir
- 115 cd .\Backup
- 116 dir
+
+Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query "CREATE TABLE invokeTable (Id TINYINT, IdData VARCHAR(5))"
+Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query "INSERT INTO invokeTable VALUES (1,'A'), (2,'B'), (3,'C'), (4,'E'),(5,'F')"
 Invoke-Sqlcmd -ServerInstance localhost -Query  "drop database YourDB"
+Invoke-Sqlcmd -ServerInstance localhost -Query "USE master; ALTER DATABASE YourDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE; DROP DATABASE YourDB"
+Restore-SqlDatabase -ServerInstance localhost -BackupFile .\yourdb.bak -Database YourDB
+Get-SqlDatabase -ServerInstance localhost
+Invoke-Sqlcmd -ServerInstance localhost -Database YourDB -Query "select * from invokeTable"
