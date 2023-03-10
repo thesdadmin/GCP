c7e1d3f9d35fe0a812ce1f4e0540920dfcffdcb2
spartacus82 <spartacus82@outlook.com>
2023-03-10 06:06:54 +0000

    update vars

--- os_policy_assignment/variables.tf
+++ os_policy_assignment/variables.tf
@@ -2,6 +2,7 @@
 
 variable "project" {
   description = "GCP Project"
+  
 }
 variable "region" {
   description = "GCP region"
@@ -15,21 +16,26 @@ variable "zone" {
 
 variable "gcs_media_bkt" {
   description = "GCS Bucket to hold scripts and PS Modules"
+  default = "test-media-bkt"
 }
 
 variable "gcs_sql_bkt_full" {
   description = "GCS Bucket for SQL Full backups"
+  default = "test-full-bkt"
 }
 
 variable "gcs_sql_bkt_diff" {
   description = "GCS Bucket for SQL DIFF Backups"
+  default = "test-diff-bkt"
 }
 
 variable "os_file_policy_assignment_name" {
   description = "File OS Policy Assignment"
+  default ="sql-file-policy"
 }
 
 variable "os_sql_policy_assignment_name" {
   description = "SQL OS Policy Assignment"
+  default = "sql-policy-backups"
 }
 
