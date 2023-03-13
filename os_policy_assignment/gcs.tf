ca5209b3d3b53e08f61316badedb73580eeadb69
spartacus82 <spartacus82@outlook.com>
2023-03-13 18:46:22 +0000

    adding count to gcs

--- os_policy_assignment/gcs.tf
+++ os_policy_assignment/gcs.tf
@@ -1,12 +1,31 @@
-
 resource "google_storage_bucket" "auto-expire" {
+  count =  var.apply_lifecycle_rule == "age" ? 1:0
   name          = "auto-expiring-bucket-lab"
   location      = "US"
   force_destroy = true
 
   lifecycle_rule {
     condition {
-      test =  var.apply_lifecycle_rule ? "age":"version"
+      age = 45
+      matches_prefix = ["*/DIFF/*.bak","*/FULL/*.bak"]
+    }
+    action {
+      type = "Delete"
+    }
+    
+  }
+
+}
+
+resource "google_storage_bucket" "auto-expire" {
+  count =  var.apply_lifecycle_rule == "version" ? 1:0  
+  name          = "auto-expiring-bucket-lab"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      
       age = 45
       matches_prefix = ["*/DIFF/*.bak","*/FULL/*.bak"]
     }
--- os_policy_assignment/provider.tf
+++ os_policy_assignment/provider.tf
@@ -1,3 +1,8 @@
+terraform {
+
+  required_version = "~> 0.12.29"
+}
+
 provider "google" {
   project = var.project
   region  = var.region
