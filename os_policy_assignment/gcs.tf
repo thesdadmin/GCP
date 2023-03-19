7d8f91244d8ef68e5a7dabd7d3f92f8588140d1c
Robert Pittman <robert.pittman.ctr@sabre.com>
2023-03-18 23:04:59 -0500

    update file policy

--- os_policy_assignment/gcs.tf
+++ os_policy_assignment/gcs.tf
@@ -1,38 +1,38 @@
 resource "google_storage_bucket" "auto-expire" {
-  count =  var.apply_lifecycle_rule == "age" ? 1:0
+  count         = var.apply_lifecycle_rule == "age" ? 1 : 0
   name          = "auto-expiring-bucket-lab"
   location      = "US"
   force_destroy = true
 
   lifecycle_rule {
     condition {
-      age = 45
-      matches_prefix = ["*/DIFF/*.bak","*/FULL/*.bak"]
+      age            = 45
+      matches_prefix = ["*/DIFF/*.bak", "*/FULL/*.bak"]
     }
     action {
       type = "Delete"
     }
-    
+
   }
 
 }
 
 resource "google_storage_bucket" "auto-expire" {
-  count =  var.apply_lifecycle_rule == "version" ? 1:0  
+  count         = var.apply_lifecycle_rule == "version" ? 1 : 0
   name          = "auto-expiring-bucket-lab"
   location      = "US"
   force_destroy = true
 
   lifecycle_rule {
     condition {
-      
-      age = 45
-      matches_prefix = ["*/DIFF/*.bak","*/FULL/*.bak"]
+
+      age            = 45
+      matches_prefix = ["*/DIFF/*.bak", "*/FULL/*.bak"]
     }
     action {
       type = "Delete"
     }
-    
+
   }
 
 }
--- os_policy_assignment/variables.tf
+++ os_policy_assignment/variables.tf
@@ -2,7 +2,7 @@
 
 variable "project" {
   description = "GCP Project"
-  
+
 }
 variable "region" {
   description = "GCP region"
@@ -16,26 +16,26 @@ variable "zone" {
 
 variable "gcs_media_bkt" {
   description = "GCS Bucket to hold scripts and PS Modules"
-  default = "test-media-bkt"
+  default     = "test-media-bkt"
 }
 
 variable "gcs_sql_bkt_full" {
   description = "GCS Bucket for SQL Full backups"
-  default = "test-full-bkt"
+  default     = "test-full-bkt"
 }
 
 variable "gcs_sql_bkt_diff" {
   description = "GCS Bucket for SQL DIFF Backups"
-  default = "test-diff-bkt"
+  default     = "test-diff-bkt"
 }
 
 variable "os_file_policy_assignment_name" {
   description = "File OS Policy Assignment"
-  default ="sql-file-policy"
+  default     = "sql-file-policy"
 }
 
 variable "os_sql_policy_assignment_name" {
   description = "SQL OS Policy Assignment"
-  default = "sql-policy-backups"
+  default     = "sql-policy-backups"
 }
 
