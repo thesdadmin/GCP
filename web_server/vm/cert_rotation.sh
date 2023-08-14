0c595f83ab2ae50244613465acb5d000d8bc8077
Bill Clay <spartacus82@outlook.com>
2023-08-14 16:05:16 -0500

    updated cert rotation-script for docker

--- web_server/vm/cert_rotation.sh
+++ web_server/vm/cert_rotation.sh
@@ -1,8 +1,38 @@
 #!/bin/bash
+
+set -euox pipefall
+CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
+CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
+
+
+echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
+
+
+# SLEEP_MS and FAIL_RATE should be a decimal
+# numbers. parse and format the input using 
+# printf. 
+#
+# printf validates the input since it 
+# quits on invalid input, as shown here:
+#
+#   $: printf '%.1f' "abc"
+#   bash: printf: abc: invalid number
+#
+SLEEP_MS=$(printf '%.1f' "${SLEEP_MS:=0}")
+FAIL_RATE=$(printf '%.1f' "${FAIL_RATE:=0}")
+
+
+gcloud storage cp $bkt_script_path/$script .
+
+chmod +x /root/$script 
+/root/$script
+
 #ENV vars from Cloud Run 
 # certificate_type (self,managed) self are user provided and managed are created by google 
 # certificate_expiration
 # 
+
+
  if certificate_type = self
       #Filter certficated list by certificates expiring by less that 15 days. The output is just the certificate name
     then
