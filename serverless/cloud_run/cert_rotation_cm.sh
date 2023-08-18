f41c9006914ad7a775841a19a941f402f819b9d1
Bill Clay <spartacus82@outlook.com>
2023-08-18 14:21:26 -0500

    refined CM rotation

---
+++ serverless/cloud_run/cert_rotation_cm.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+set -euox pipefall
+CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
+CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
+
+
+echo "Starting Task #${CLOUD_RUN_TASK_INDEX}, Attempt #${CLOUD_RUN_TASK_ATTEMPT}..."
+
+
+# SLEEP_MS and FAIL_RATE should be a decimal
+# numbers. parse and format the input using 
+# printf. 
+#
+# printf validates the input since it 
+# quits on invalid input, as shown here:
+#
+#   $: printf '%.1f' "abc"
+#   bash: printf: abc: invalid number
+#
+SLEEP_MS=$(printf '%.1f' "${SLEEP_MS:=0}")
+FAIL_RATE=$(printf '%.1f' "${FAIL_RATE:=0}")
+
+ #If there are multiple certificates for different domains, we need to add a "AND" to the filter statement and parse by domain name
+RESULT=$(gcloud certificate-manager certificates list --filter="expireTime < P15D" --format="value(name)")
+
+project_number=$(gcloud config list --format='value(core.project)')
+RESULT=$(gcloud certificate-manager certificates list --filter="expireTime =< P15D" --format="value(name)")
+if [ $? -ne 0 ];
+   then 
+     echo "Certificate $RESULT needs to be removed from the target_https_proxies"    
+     #Identify new certificate
+     newcertificate=gcloud certificate-manager certificates list --filter="expireTime > P60D" --format="value(name)"
+     
+     echo "$newcertificate needs to be mapped to target-https-proxy"
+
+     oldcertificate=$(gcloud certificate-manager certificates list --filter= "expireTime < P15D" --format="value(name)")
+     
+     echo "Identify map $RESULT is attached too"
+     
+     #Identify Proxy with DNS-MAP
+     
+     $map_list = $(gcloud certificate-manager maps list --format 'value(name)')
+     for map in $map_list 
+     do 
+        gcloud certificate-manager maps entries list --map $map --filter certificates="projects/$project_number/locations/global/certificates/$oldcertificate"
+        if [ $? -eq 0 ]
+            then
+            $name=$(gcloud certificate-manager maps entries list --map dns-map --filter certificates="projects/$project_number/locations/global/certificates/$oldcertificate" --format='value(name)'
+
+            echo "Add new certificate to Certificate Entry $name for Certificate Map $map"
+
+            gcloud certificate-manager maps entries update $name --map $map --certificates "$oldcertificate","$newcertificate"
+            
+            echo "Waiting for new certificate to propagate"
+
+            sleep 30m
+            
+            gcloud certificate-manager maps entries update $name --map $map --certificates "$newcertificate"
+
+        else 
+            echo "No matching certficate in $map"
+        fi 
+        echo 
+    done 
+            
+
+
+     sleep 30m
+
+     
+
+    else
+
+    echo "no certificate to rotate"
+fi
--- serverless/cloud_run/cert_rotation_comute.sh
+++ serverless/cloud_run/cert_rotation_comute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -euox pipefall
+set -euox pipefail
 CLOUD_RUN_TASK_INDEX=${CLOUD_RUN_TASK_INDEX:=0}
 CLOUD_RUN_TASK_ATTEMPT=${CLOUD_RUN_TASK_ATTEMPT:=0}
 
@@ -26,9 +26,9 @@ FAIL_RATE=$(printf '%.1f' "${FAIL_RATE:=0}")
 
       #Filter certficated list by certificates expiring by less that 15 days. The output is just the certificate name
 
-RESULT=gcloud compute ssl-certificates list --filter="expireTime < P15D" --format="value(name)"
+RESULT=$(gcloud compute ssl-certificates list --filter="expireTime < P15D" --format="value(name)" )
 
-if [$RESULT -ne $null];
+if [ $? -ne 0 ];
       then 
       old_certificate=$(gcloud compute ssl-certificates list --filter="expireTime < P15D" --format="value(name)")
 
@@ -40,19 +40,28 @@ if [$RESULT -ne $null];
       # gcloud certificate-manager certificates list --filter="expireTime > P30D and managed.domains = 'SOMEDOMAIN'" --format="value(name)"
       
       
-      $old_link = $(gcloud compute ssl-certificates list --filter="expireTime < P15D" --format="value(selfLink)")
+      old_link = $(gcloud compute ssl-certificates list --filter="expireTime < P15D" --format="value(selfLink)")
 
+      echo "Find proxy with attached certificate"
+
+      $target_proxy=$(gcloud compute target-https-proxies list --filter ssl_certificates="$old_link")
       
-      gcloud compute target-https-proxies list --filter ssl_certificates="$old_link"
+      echo "Add new certificate to proxy"
+
+      gcloud compute target-ssl-proxies SOME_PROXY_NAME update --ssl-certificates '$old_certificate','$new_certificate'
       
-      gcloud compute target-ssl-proxies update --ssl-certificates '$old_certificate','$new_certificate'
-      
+      echo "Wait for new connections"
+
       sleep 2m
 
+      echo "Remove old certificate"
+      
       gcloud compute target-ssl-proxies SOME_PROXY_NAME update --ssl-certificates '$new_certificate'
   
   
 
 else 
+
   echo "No certficates to rotate"  
+
 fi
